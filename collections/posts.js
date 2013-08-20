fa963c20e08a79e7930c8f2027259830e5c12962
lindsayrattray <lindsayrattray@yahoo.com>
2013-08-20 15:41:20 +1000

    Commit 7-7

--- collections/posts.js
+++ collections/posts.js
@@ -11,7 +11,7 @@ Meteor.methods({
 
     // ensure that the post has a title
     if(!postAttributes.title)
-      throw new Meteor(422, "Please fill in a headline");
+      throw new Meteor.Error(422, "Please fill in a headline");
 
     // check  that there are no previous posts with the same link
     if (postAttributes.url  && postWithSameLink)
